9013b35dc37c140cbcae7385735ecf7839ad67f3
wilhelmkindly <wilhelmkindly@gmail.com>
2025-09-14 19:08:16 +0300

    Fix

--- frontend/page_object/account_login_page.py
+++ frontend/page_object/account_login_page.py
@@ -10,6 +10,7 @@ class AccountLoginPage(BasePage):
     INPUT_EMAIL = "//input[contains(@name, 'email')]"
     INPUT_PASSWORD = "//input[contains(@name, 'password')]"
     BUTTON_LOGIN = "//button[contains(text(), 'Login')]"
+    ELEM_MY_ACCOUNT = "//h2[contains(text(), 'My Account')]"
 
     def __init__(self):
         super().__init__()
@@ -23,3 +24,5 @@ class AccountLoginPage(BasePage):
         self.data_entry(browser=browser, target=self.INPUT_EMAIL, value=email)
         self.data_entry(browser=browser, target=self.INPUT_PASSWORD, value=password)
         self.wait_and_click(browser=browser, target_locator=self.BUTTON_LOGIN)
+        self.wait_element(browser=browser, target_locator=self.ELEM_MY_ACCOUNT)
+
