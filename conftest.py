f44c34e92b132ebdbc1024b038f208352d16523a
wilhelmkindly <wilhelmkindly@gmail.com>
2025-09-15 03:10:17 +0300

    fix

--- conftest.py
+++ conftest.py
@@ -1,5 +1,6 @@
 import pytest
 import allure
+import random
 import requests
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options as FFoptions
@@ -68,11 +69,7 @@ def browser(request):
                 options.add_argument("headless=new")
             options.add_argument("--no-sandbox")
             options.add_argument("--disable-dev-shm-usage")
-
-            options.add_argument("--incognito")
-            options.add_argument("--no-default-browser-check")
-            options.add_argument("--no-first-run")
-
+            options.add_argument(f"--user-data-dir=/tmp/chrome_{random.randint(1, 10000)}")
             driver = webdriver.Chrome(options=options)
         elif browser_name in ["ff", "firefox"]:
             options = FFoptions()
